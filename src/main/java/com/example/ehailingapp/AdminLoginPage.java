412c2aee7ba0bdd6792ae6ad60fa0ad55f48e257
JackTZY <jacktzy7899@gmail.com>
2022-05-21 17:23:58 +0800

    Initial commit

--- src/main/java/com/example/ehailingapp/AdminLoginPage.java
+++ src/main/java/com/example/ehailingapp/AdminLoginPage.java
@@ -25,7 +25,7 @@ public class AdminLoginPage extends Application {
         stage.show();
     }
 
-    public static void main(String[] args) {
+    public static void accessAdminLoginPage() {
         launch();
     }
 }
--- src/main/java/com/example/ehailingapp/AdminMain.java
+++ src/main/java/com/example/ehailingapp/AdminMain.java
@@ -5,19 +5,19 @@ import java.sql.DriverManager;
 
 public class AdminMain {
     public static void main(String[] args) {
+        Connection conn;
         try{
             String driver = "com.mysql.cj.jdbc.Driver";
             String url = "jdbc:mysql://localhost:3306/maya2";
-            String username = "root";
-            String password = "jack55092080";
+            String username = ""; //username
+            String password = ""; //password
             Class.forName(driver);
 
-            Connection conn = DriverManager.getConnection(url,username,password);
+            conn = DriverManager.getConnection(url,username,password);
             //System.out.println("Connected");
-            return conn;
         }catch(Exception e){
             System.out.println(e);
-            return null;
         }
+        AdminLoginPage.accessAdminLoginPage();
     }
 }
