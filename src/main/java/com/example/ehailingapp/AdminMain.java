a0d8189e9c537537f16ce3b8427c3e556f20a244
Chongzy5419 <u2102854@siswa.um.edu.my>
2022-05-22 22:40:42 +0800

    added the problem file

---
+++ src/main/java/com/example/ehailingapp/AdminMain.java
@@ -0,0 +1,28 @@
+package com.example.ehailingapp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class AdminMain {
+    public static void main(String[] args) throws Exception {
+        //Connection conn = getConnection();
+        AdminLoginPage.accessAdminLoginPage();
+    }
+
+    public static Connection getConnection() throws Exception{
+        try{
+            String driver = "com.mysql.cj.jdbc.Driver";
+            String url = "jdbc:mysql://localhost:3306/xxxx";
+            String username = ""; //username
+            String password = ""; //password
+            Class.forName(driver);
+
+            Connection conn = DriverManager.getConnection(url,username,password);
+            //System.out.println("Connected");
+            return conn;
+        }catch(Exception e){
+            System.out.println(e);
+            return null;
+        }
+    }
+}
