1e1ffdcd7f599643c39efa9bf3e8fe93d09d3897
Chongzy5419 <u2102854@siswa.um.edu.my>
2022-05-30 20:33:14 +0800

    added the conn

--- src/main/java/DbAccess/main.java
+++ src/main/java/DbAccess/main.java
@@ -2,7 +2,7 @@ package DbAccess;
 
 public class main {
     public static void main(String[] args) throws Exception {
-        conn.createAcc("hii","118","1118");
-        conn.UserLogin("hi","1118");
+        Uconn.createAcc("hii","118","1118");
+        Uconn.UserLogin("hi","1118");
     }
 }
